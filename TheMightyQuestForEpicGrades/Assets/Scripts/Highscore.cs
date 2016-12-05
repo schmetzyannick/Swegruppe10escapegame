c5e4ac7d76371c77d7a16464e62788c665902da3
schmetzyannick <schmetzyannick@gmail.com>
2016-12-05 14:50:24 +0100

    InsertHighscore
    Hier wurde eine neue Scene (Highscore) eingeführt sie sollte aufgerufen
    werden, wenn das letzte Level geschafft wurde und man mit seinem Score
    in den Top 10 landet. Hinzu kommt ein neues Script(Highscore) zum
    abspeichern des Scores.
    Der Aufruf der Scene durch eine andere ist nicht dabei.

---
+++ TheMightyQuestForEpicGrades/Assets/Scripts/Highscore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Assets.Scripts;
+using UnityEngine.SceneManagement;
+
+public class Highscore : MonoBehaviour {
+
+    public Text name { get; set; }
+    public Text zeit { get; set; }
+    public Text punkte { get; set; }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void NextWindowOnClick(int level)
+    {
+        Persist.save<Highscore>(this);
+        SceneManager.LoadScene(level);
+    }
+
+}
